103438832e783e965f1c341d6102a45cc8cd2506
daniel-mahmoodi <mahmoodidanial2@gmail.com>
2021-08-13 02:18:03 +0430

    side bar home

--- js/megaMenu.js
+++ js/megaMenu.js
@@ -1,3 +1,5 @@
+// megaMenu
+
 $(document).ready(function() {
     $(".dropdown").hover(function() {
         var dropdownMenu = $(this).children(".dropdown-menu");
@@ -6,3 +8,25 @@ $(document).ready(function() {
         }
     });
 });
+// end megaMenu
+
+// side menu
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '80%';
+    document.getElementById('grayBgSlideNav').style.width = '100%';
+
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('grayBgSlideNav').style.width = '0';
+    document.getElementById('openHomeSide-Menu').style.width = '0';
+}
+
+function openHomeMenu() {
+    document.getElementById('openHomeSide-Menu').style.width = '80%';
+
+}
+
+
+// end side menu
